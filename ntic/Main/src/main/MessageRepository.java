45662f5adacdf25d4b1c35091703174de0692fb3
khelalfasel-afk <khelalfaselssabil@gmail.com>
2026-03-26 18:01:21 +0100

    Update MessageRepository.java

--- ntic/Main/src/main/MessageRepository.java
+++ ntic/Main/src/main/MessageRepository.java
@@ -7,22 +7,7 @@ import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-
-
-
-
-
-
-
-
-
-
-
 @Repository
-
-
-
-
 public interface MessageRepository extends JpaRepository<Message, MessageId> {
     List<Message> findByExpediteurEmailAndDestinataireEmailOrExpediteurEmailAndDestinataireEmailOrderByCreatedAtAsc(String expediteurEmail1, String destinataireEmail1, String expediteurEmail2, String destinataireEmail2);
 
